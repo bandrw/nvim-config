f8a137928548b5dcd4d586cd62926d291dc03c77
bandrw <andr.bal@icloud.com>
2025-10-24 15:44:35 +0800

    update telescope

--- lua/bandrw/colorscheme.lua
+++ lua/bandrw/colorscheme.lua
@@ -1,8 +1,6 @@
 vim.cmd("colorscheme tokyonight")
 
 require("tokyonight").setup({
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
 	style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 	light_style = "day", -- The theme is used when the background is set to light
 	transparent = false, -- Enable this to disable setting the background color
@@ -35,3 +33,5 @@ require("tokyonight").setup({
 	---@param colors ColorScheme
 	on_highlights = function(highlights, colors) end,
 })
+
+-- vim.o.background = "light"
